a6250cdaaeec21580fc76469f04fba0491f9983e
chachaki <sasaki@guildworks.jp>
2017-01-10 12:42:24 +0900

    fix typo

--- lib/omniauth/strategies/office365.rb
+++ lib/omniauth/strategies/office365.rb
@@ -32,7 +32,7 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get('"https://outlook.office.com/api/v2.0/me/').parsed
+        @raw_info ||= access_token.get("https://outlook.office.com/api/v2.0/me/").parsed
       end
     end
   end
